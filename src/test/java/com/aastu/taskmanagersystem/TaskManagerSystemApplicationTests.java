c24e591b955d7cb1e972be25f470643c2089bfd7
selman <awelselman3@gmail.com>
2026-06-01 05:54:59 +0300

    Initial project structure

---
+++ src/test/java/com/aastu/taskmanagersystem/TaskManagerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aastu.taskmanagersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
